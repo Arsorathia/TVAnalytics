ce1caf198f050f2f3d62e6b27f62c3f41694dee6
Arif Sorathia <arifsorathia@Arifs-MacBook-Pro-3.local>
2015-01-25 20:17:34 -0500

    Added Twitter Stream

--- modules/browse/controllers.js
+++ modules/browse/controllers.js
@@ -1,15 +1,32 @@
 ﻿'use strict';
 
-angular.module('Browse')
+angular.module('Browse', ['ngRoute'])
 
-.controller('BrowseController',
-    ['$scope', '$rootScope','$cookies','$cookieStore' ,
-    function ($scope, $rootScope, $cookies,$cookieStore) {
+  .factory('socket', function($rootScope){
+   var socket = io.connect('http://localhost:8080');
+    return {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        }
+    };
+  })
+
+  .controller('BrowseController', ['$scope', 'socket', function ($scope, socket) {
     $scope.projects = [
 			{id:1, Name:'Project 1',  Sow:"SOW 1" ,Partner: "Part A", Sig: "Sig A" },
 			{id:2, Name:'Project 2',  Sow:"SOW 2" ,Partner: "Part A", Sig: "Sig A"},
 			{id:3, Name:'Project 3',  Sow:"SOW 3" ,Partner: "Part B", Sig: "Sig B"},
 			{id:4, Name:'Project 4',  Sow:"SOW 4" ,Partner: "Part A", Sig: "Sig A"},
 			{id:5, Name:'Project 5',  Sow:"SOW 5" ,Partner: "Part C", Sig: "Sig C"},
-			{id:6, Name:'Project 6',  Sow:"SOW 6" ,Partner: "Part A", Sig: "Sig A"}]
-    }]);
+			{id:6, Name:'Project 6',  Sow:"SOW 6" ,Partner: "Part A", Sig: "Sig A"}];
+    $scope.tweets = [];
+ 
+socket.on('tweets', function (data) {
+    $scope.tweets = $scope.tweets.concat(data);
+});
+  }])
--- server.js
+++ server.js
@@ -5,6 +5,14 @@ var app = express();
 var server = app.listen(8080);
 var io = require('socket.io').listen(server);
 exports.io = io;
+ 
+app.all('*', function(req, res, next) {
+  	res.set('Access-Control-Allow-Origin', '*');
+  	res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+  	res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  	if ('OPTIONS' == req.method) return res.send(200);
+  	next();
+});
 
 
   app.use(express["static"](__dirname));
